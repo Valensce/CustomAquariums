d06871cc8f11ddbbcaf365a2663910de1ed50295
Ｖｉｃｔｏｒｉｏｕｓ <pradolifu@gmail.com>
2021-08-08 22:19:07 +1000

    Update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -82,3 +82,11 @@ Use this link to help https://www.w3schools.com/js/tryit.asp?filename=tryjs_form
 Also reset the output (tdCost). Use this link to help you do this
 https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_innerhtml_delete
 */
+
+home = function() {
+	alert("You are currently in our home page!");
+};
+
+news = function () {
+  alert("This is currently under progress, but Prado says it'll be complete soon 🐶")
+}
